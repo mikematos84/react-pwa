82a3dac69b7889948ba388d075c493be32f84391
Mike Matos <mmatos@ets.org>
2020-02-26 14:20:10 -0500

    Added add custom install

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [showInstallPromotion, setShowInstallPromotion] = useState(false);
+
+  useEffect(() => {
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      console.log("beforeinstallprompt")
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      setDeferredPrompt(e);
+      // Update UI notify the user they can install the PWA
+      setShowInstallPromotion(true);
+    });
+
+    window.addEventListener('appinstalled', (evt) => {
+      console.log('a2hs installed');
+    });
+
+  }, []);
+
+  const installApp = () => {
+    // Hide the app provided install promotion
+    setShowInstallPromotion(false);
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+    })
+  }
+
   return (
     <div className="App">
+      {
+        showInstallPromotion &&
+        <div>
+          <button onClick={installApp}>Install</button>
+        </div>
+      }
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
