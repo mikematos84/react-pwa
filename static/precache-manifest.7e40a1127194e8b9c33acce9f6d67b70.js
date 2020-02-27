0b3fb7e43e077fe0f9e94ebac64c26b7859b4041
Mike Matos <mmatos@ets.org>
2020-02-27 11:11:10 -0500

    Updates

---
+++ static/precache-manifest.7e40a1127194e8b9c33acce9f6d67b70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e6544508fa4c05397b9a435b6bc52c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0923055c498d9e6f0c1f",
+    "url": "/static/css/main.4dda1af7.chunk.css"
+  },
+  {
+    "revision": "4e84db83558321acf012",
+    "url": "/static/js/2.36532ec8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.36532ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0923055c498d9e6f0c1f",
+    "url": "/static/js/main.d4096a3e.chunk.js"
+  },
+  {
+    "revision": "e1405c2239592115999c",
+    "url": "/static/js/runtime-main.3630572f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
