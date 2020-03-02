88a6181227d8d80e4c8b39ac2b947168bbf7fb94
Mike Matos <mmatos@ets.org>
2020-03-02 11:32:19 -0500

    Fixed MediaRecorder error by resetting/creating a new instance of a media player on stop

--- src/Recorder.js
+++ src/Recorder.js
@@ -7,68 +7,142 @@ const Recorder = props => {
   const recorderRef = useRef();
   const playerRef = useRef();
 
+  /**
+   * Initialize the MediaRecorder on component mount
+   */
   useEffect(() => {
-    enableRecorder();
+    initializeMediaRecorder();
   }, [])
 
+  /**
+   * Upon MedaRecorder being set, monitor the following events
+   */
   useEffect(() => {
     let chunks = [];
 
-    mediaRecorder.ondataavailable = (e) => {
-      chunks.push(e.data);
-    }
-
+    mediaRecorder.ondataavailable = e => chunks.push(e.data)
     mediaRecorder.onstop = (e) => {
       let blob = new Blob(chunks, { type: 'video/mp4' });
       chunks = [];
       let url = (window.URL || window.webkitURL).createObjectURL(blob);
-      console.log(url);
-      playerRef.current.src = url;
+      setPlaybackPreview(url);
     }
 
-    console.log('MediaRecorder Ready');
+    console.log('MediaRecorder ready');
   }, [mediaRecorder]);
 
+  /**
+   * Helper function to console out change in state of the MediaRecorder
+   */
   useEffect(() => {
-    if (mediaRecorder.state !== undefined)
-      console.log(mediaRecorder.state)
-  }, [mediaRecorder, isRecording])
+    console.log(`MediaRecorder state: ${mediaRecorder.state}`)
+  }, [mediaRecorder.state])
 
-  const start = () => {
-    if (isRecording) return;
-    mediaRecorder.start(1000);
+  /**
+   * Start recording the stream
+   */
+  const start = async () => {
+    if (mediaRecorder.state === 'recording') return;
+    mediaRecorder.start();
     setIsRecording(true);
   }
 
-  const stop = () => {
-    if (!isRecording) return;
+  /**
+   * Stop recording the stream
+   */
+  const stop = async () => {
+    if (mediaRecorder.state === 'inactive') return;
     mediaRecorder.stop();
     setIsRecording(false);
+    await initializeMediaRecorder();
   }
 
-  const enableRecorder = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: {
-          facingMode: 'user'
-        }
-      });
+  /**
+   * Set the playback player's source to the url of the newly recorderd stream
+   * @param {string} url 
+   */
+  const setPlaybackPreview = url => {
+    if (!playerRef.current) return;
+    console.log(`Playback URL: ${url}`);
+    playerRef.current.src = url;
+  }
 
-      setMediaRecorder(new MediaRecorder(stream));
-      recorderRef.current.srcObject = stream;
-      console.log("MediaRecorder Enabled")
-    }
-    catch (err) {
-      console.log(err);
-    }
+  /**
+   * Get a media device stream (webcam)
+   */
+  const getStream = () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: {
+            facingMode: 'user'
+          }
+        });
+        console.log('Stream fetched');
+        resolve(stream);
+      }
+      catch (err) {
+        console.log('Error in fetching stream')
+        reject(err);
+      }
+    })
+  }
+
+  /**
+   * Set the live stream retrieved from the media device
+   * to the designated player to preview
+   * @param {object} stream 
+   */
+  const setRecordingStreamPreview = stream => {
+    if (!recorderRef.current) return;
+    recorderRef.current.srcObject = stream;
+  }
+
+  /**
+   * Create MediaRecorder object from a given stream
+   * @param {object} stream 
+   */
+  const createMediaRecorder = stream => {
+    return new Promise((resolve, reject) => {
+      try {
+        const mediaRecorder = new MediaRecorder(stream);
+        console.log('New MediaRecorder created');
+        resolve(mediaRecorder);
+      }
+      catch (err) {
+        console.log('Error in creating new MediaRecorder');
+        reject(err);
+      }
+    })
+  }
+
+  /**
+   * Initialize MediaRecorder
+   */
+  const initializeMediaRecorder = async () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const stream = await getStream();
+        setRecordingStreamPreview(stream);
+        const mediaRecorder = await createMediaRecorder(stream);
+        setMediaRecorder(mediaRecorder);
+        resolve(mediaRecorder);
+      }
+      catch (err) {
+        console.log('Error in initializing MediaRecorder of fetching media devices stream')
+        reject(err);
+      }
+    })
   }
 
   return (
     <React.Fragment>
       <div>
-        {!isRecording && <button onClick={start}>Start Recording</button>}
-        {isRecording && <button onClick={stop} className={'recording'}>Stop Recodring</button>}
+        <button
+          onClick={isRecording ? stop : start}
+          className={isRecording ? 'recording' : ''}
+        >{isRecording ? 'Stop' : 'Start'} Recording </button>
       </div>
       <div>
         <h3>Recorder</h3>
