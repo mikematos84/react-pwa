c6fa41dd0932d0b6ce1d9ab8c40f968db15da58f
Mike Matos <mmatos@ets.org>
2020-03-06 17:30:20 -0500

    Updates

---
+++ precache-manifest.180cf8d01bdb80e198d8a26ebf40fc3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5f08f6bd33daf4d8e0db2bc860432d",
+    "url": "https://mikematos84.github.io/react-pwa/index.html"
+  },
+  {
+    "revision": "7764e9185a0f354627f5",
+    "url": "https://mikematos84.github.io/react-pwa/static/css/main.893c6bda.chunk.css"
+  },
+  {
+    "revision": "5c53db01fa9c41ed04a7",
+    "url": "https://mikematos84.github.io/react-pwa/static/js/2.a3ec7a19.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "https://mikematos84.github.io/react-pwa/static/js/2.a3ec7a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7764e9185a0f354627f5",
+    "url": "https://mikematos84.github.io/react-pwa/static/js/main.7ef48cf9.chunk.js"
+  },
+  {
+    "revision": "882279db44685b084e23",
+    "url": "https://mikematos84.github.io/react-pwa/static/js/runtime-main.644230bb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-pwa/precache-manifest.e142c4ad2023d486bfedab6ff920e0eb.js"
+  "https://mikematos84.github.io/react-pwa/precache-manifest.180cf8d01bdb80e198d8a26ebf40fc3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-pwa/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://mikematos84.github.io/react-pwa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
